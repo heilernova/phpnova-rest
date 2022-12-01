6436c55f45735feadf1e8f2ce4db03017085d279
Heile Nova <heilernova@outlook.com>
2022-12-01 14:56:52 -0500

    Corrección de nombre de variables

--- src/Router/Funcs/nv-router-run.php
+++ src/Router/Funcs/nv-router-run.php
@@ -43,7 +43,7 @@ function nv_router_run(string $url, string $method): Response
         case "multipart/form-data":
             if ($method == 'POST' && $method != 'GET'){
                 $body = array_map(array: $_POST, callback: function($item){
-                    if ( preg_match('/^\{?.+\}/', $$item) > 0 || preg_match('/^\[?.+\]/', $$item) > 0){
+                    if ( preg_match('/^\{?.+\}/', $item) > 0 || preg_match('/^\[?.+\]/', $item) > 0){
                         $json = json_decode($item);
                         return json_last_error() == JSON_ERROR_NONE ? $json : $item;
                     }
